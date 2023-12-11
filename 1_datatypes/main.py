ff25e0afca88ae0ed540909a677b5e8b693b330f
inderjit-fullstack-dev <inderjit.fullstack.dev@gmail.com>
2023-12-11 17:34:25 +0530

    arithmetic expression

---
+++ 1_datatypes/main.py
@@ -0,0 +1,26 @@
+print('Hello World')
+
+# arithmetic operators
+
+# Addition
+print(2 + 2)
+
+# Subtraction
+print(2 - 2)
+
+# Addition
+print(2 * 2)
+
+# Division
+print(2 / 2)
+
+# Modules
+print(2 % 2)
+
+# Power
+print(2 ** 2)
+# Expression
+print(10 + 5 * 40 - 4)
+
+# Expression Priority with ()
+print((10 + 5) * (40 - 4))
